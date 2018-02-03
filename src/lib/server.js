5ea6c752166ad71f2f6c5c27871cf1efb93d870f
Pmookerjee <pmookerjee@gmail.com>
2018-02-02 19:44:44 -0800

    Some console.logs in server file

--- src/lib/server.js
+++ src/lib/server.js
@@ -19,7 +19,7 @@ app.use('*', (req, res, next) => {
   res.header('Access-Control-Allow-Credentials',  true);
   req.header('Access-Control-Request-Headers', 'Authorization, Content-Type')
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD');
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
