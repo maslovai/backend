111836c49c3fed6d822e4de8f8c1e765880601a6
Pmookerjee <pmookerjee@gmail.com>
2018-02-06 19:29:58 -0800

    updates to model

--- src/model/group.js
+++ src/model/group.js
@@ -5,7 +5,8 @@ import Mongoose, {Schema} from 'mongoose';
 const groupSchema = Mongoose.Schema({
   name: {type: String, required: true},
   alias: {type: String},
-  user_IDs: {type: Array}
+  user_IDs: {type: Array},
+  tasks: {type: Array}
 })
 
 const Group = Mongoose.model('group', groupSchema);
