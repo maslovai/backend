2ef777fac690658066a987b6860514309227eef8
Rowen Scout <rowenscout@gmail.com>
2018-02-07 20:21:16 -0800

    removed unneeded comment

--- src/router/group.js
+++ src/router/group.js
@@ -10,7 +10,6 @@ import namor from 'namor';
 const groupRouter = module.exports = express.Router();
 
 groupRouter.put('/group', bearer, bodyParser.json(), (req, res, next) => {
-  //add createdBy to group and set to user._id
 
   //it needs req.body.name which becomes the name for the new group
   //it needs req.user._id to locate and update the user with the new groupID
