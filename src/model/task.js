9fef32d32d418fe62bd720d73f13329ff1bba39f
Iryna Maslova <maslovai@hotmail.com>
2018-02-09 20:12:05 -0800

    for Rowen

--- src/model/task.js
+++ src/model/task.js
@@ -8,7 +8,7 @@ const taskSchema = new Schema({
   groupName: {type: String, required: false },
   completed: {type: Boolean, default: false},
   completedBy: {type: String, required: false},
-  createDate: {type: Date, default: Date.now(), expireAfterSeconds:1000*60*5}
+  createDate: {type: Date, default: Date.now()}
 })
 
 const Task = Mongoose.model('task', taskSchema);
--- src/router/stats.js
+++ src/router/stats.js
@@ -17,22 +17,31 @@ console.log('in backend, stats route ', req.params)
   Group.findOne({_id: req.params.id})
     .then(group => {
       console.log('::::::::::::', group)
-      group.user_ID.forEach(user).map(userID => {
-        table.push({_id: user.userID})
+      group.user_IDs.forEach(userID => {
+        table.push({"id":userID})
       })
+      console.log("table: ", table)
+      return table
     })
-   console.log('::::::::::::', table)
+    .then(table =>{
+      console.log("in last then: ",table)
+      table.forEach( (obj, i) => {
+        User.find({_id: obj.id})
+          .then(user => {
+            let tableLength = 0;
+            if(user.completedTasks) retval + user.completedTasks.length();
+            table[i].value = tableLength;
+            table[i].label = user.firstName;
+          })   
+      })
+      res.send(table)
+    })
+    .catch(next);
+})
+    
+  //  console.log('::::::::::::', table)
     // <Piechart x={100} y={100} outerRadius={100} innerRadius={50}
     //           data={[{value: 92-34, label: 'Code lines'},
     //                  {value: 34, label: 'Empty lines'}]} />
 
-    table.map( userID, i => {
-      User.find({_id: userID})
-        .then(user => {
-          table[i].value = user.completed;
-          table[i].label = user.username;
-        })
-    })
-  res.send(table);
-
-})
+   
